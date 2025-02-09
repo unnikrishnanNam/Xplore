28ed5c3618a10a87a14c9723ffe33bed6824c0c1
Unnikrishnan Namboothiri <unnikrishnan.professional@gmail.com>
2025-02-09 16:47:12 +0530

    Add utility functions, initialized shadcnUI, update TypeScript configuration, and implement Button component with variants

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
-    <div className="h-screen w-screen bg-slate-200">
+    <div className="h-screen w-screen bg-slate-200 flex flex-col justify-center items-center gap-2">
       <h1 className="text-center text-4xl font-bold">Hello World</h1>
+      <Button>It Works</Button>
     </div>
   );
 }
